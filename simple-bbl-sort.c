eba30784aadcb15001e3f0cd2e8c0061c7d9b0e7
Jorkata Zah <gzaharievwork@gmail.com>
2025-02-07 19:10:58 +0200

    Project added

---
+++ simple-bbl-sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// I hate this way of structuring code but i can't come up with a better way
+void sort(int array[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Now let's log it
+void print(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d", array[i]);
+    }
+}
+
+int main()
+{
+    int array[] = {1, 4, 7, 2, 5, 3, 9, 6, 8, 0, 2, 1, 3, 4, 5, 7, 8, 5, 7, 0, 8, 6, 3, 1, 3, 0, 5, 3, 7, 3,2, 4,3, 4,5, 6,5, 6,7, 9,8, 1,2, 2};
+    int size = sizeof(array) / sizeof(array[0]);
+
+    sort(array, size);
+    print(array, size);
+
+    return 0;
+}
